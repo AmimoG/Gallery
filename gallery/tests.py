282aa1e8bcdde8dfe397ea74e4a986b89fe8ac83
AmimoG <gilbertmatete6@gmail.com>
2020-10-10 10:39:37 +0300

    Add Files

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,111 +1,45 @@
 from django.test import TestCase
+from .models import Editor,Article,tags
+import datetime as dt
 
-from .models import Image,Category,Location
 # Create your tests here.
+class EditorTestClass(TestCase):
 
-class LocationTestCLass(TestCase):
-    #Set up Method
+    # Set up method
     def setUp(self):
-        self.loc = Location(name="Africa")
-        self.loc.save_location()
+        self.amimo= Editor(first_name = 'Amimo', last_name ='Matete', email ='gilbertmatete6@gmail.com')
 
+    # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.loc,Location))
+        self.assertTrue(isinstance(self.amimo,Editor))
 
-    def test_save_method(self):
-        self.loc.save_location()
-        locations = Location.objects.all()
-        self.assertTrue(len(locations) > 0)
+class ArticleTestClass(TestCase):
 
-    def test_delete_method(self):
-        self.loc.save_location()
-        self.loc.delete_location()
-        location = Location.objects.all()
-        self.assertTrue(len(location) == 0)
-
-    def test_update(self):
-        location = Location.get_location_id(self.loc.id)
-        location.update_location('Donholm')
-        location = Location.get_location_id(self.loc.id)
-        self.assertTrue(location.name == 'Donholm')
-
-
-
-class CategoryTestClass(TestCase):
-    # Set up Method
     def setUp(self):
-        self.cat = Category(name="fashion")
-        self.cat.save_category()
+        # Creating a new editor and saving it
+        self.amimo= Editor(first_name = 'Amimo', last_name ='Matete', email ='gilbertmatete6@gmail.com')
+        self.amimo.save_editor()
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.cat, Category))
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
 
-    def test_save_method(self):
-        self.cat.save_category()
-        category = Category.objects.all()
-        self.assertTrue(len(category) > 0)
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.amimo)
+        self.new_article.save()
 
-    def test_delete_method(self):
-        self.cat.save_category()
-        self.cat.delete_category()
-        category = Category.objects.all()
-        self.assertTrue(len(category) == 0)
-
-    def test_update(self):
-        category = Category.get_category_id(self.cat.id)
-        category.update_category('Travel')
-        category = Category.get_category_id(self.cat.id)
-        self.assertTrue(category.name == 'Travel')
-
-
-
-class ImageTestClass(TestCase):
-    # Set up Method
-    def setUp(self):
-        self.cat = Category(name="fashion")
-        self.cat.save_category()
-
-        self.loc = Location(name="Africa")
-        self.loc.save_location()
-
-        self.image = Image(name='image test', description='my test',image_location=self.loc, image_category=self.cat)
-        self.image.save_image()
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.image, Image))
+        self.new_article.tags.add(self.new_tag)
 
     def tearDown(self):
-        self.image.delete_image()
-        self.cat.delete_category()
-        self.loc.delete_location()
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
 
-
-    def test_save_method(self):
-        self.image.save_image()
-        images  = Image.objects.all()
-        self.assertTrue(len(images)>0)
-
-
-    def test_get_all_images(self):
-        images = Image.get_all_images()
-        self.assertTrue(len(images)>0)
-
-    def test_get_image_by_id(self):
-        images= Image.get_image_by_id(self.image.id)
-        self.assertTrue(len(images) == 1)
-
-    def test_search_by_category(self):
-        images = Image.search_by_category('fashion')
-        self.assertTrue(len(images)>0)
-
-    def test_filter_by_location(self):
-        images = Image.fil0ter_by_location('1')
-        print(images)
-        self.assertTrue(len(images)>0)
-
-    def test_update_image(self):
-        self.image.save_image()
-        image = Image.update_image( self.image.id, 'test update', 'my test',self.loc, self.cat)
-        upimage = Image.objects.filter(id = self.image.id)
-        print(upimage)
-        self.assertTrue(Image.name == 'test update')
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news)>0)
+        
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
--- mygallery/urls.py
+++ mygallery/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'',include('gallery.urls'))
+    path('',include('news.urls'))
 ]
