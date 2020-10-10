d70fa0cb053f8d889b86a28843e31453885d32fb
AmimoG <gilbertmatete6@gmail.com>
2020-10-10 09:14:02 +0300

    Add Files

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -111,8 +111,10 @@ WSGI_APPLICATION = 'mygallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'moringa',
+    'PASSWORD':'Gamatete6',
     }
 }
 
